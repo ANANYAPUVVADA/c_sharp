c2d52e75c68a55b103a4fb2a487cbf647be65b3f
ANANYAPUVVADA <130781442+ANANYAPUVVADA@users.noreply.github.com>
2023-09-13 11:39:28 +0530

    Create arithematicoperators.cs

---
+++ arithematicoperators.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithemtic_operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10, y = 20;
+            int a = x + y;
+            int b = x - y;
+            int c = a * b;
+            double d = c / b;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+        }
+    }
+}
